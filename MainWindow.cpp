1f8d69a3bf33b39cd7f357340080c8654b27f247
Сайед Кирилл <sayed.kirill@gmail.com>
2020-05-03 21:34:41 +0300

    Added log and commands

--- MainWindow.cpp
+++ MainWindow.cpp
@@ -1,15 +1,39 @@
 #include "MainWindow.h"
 #include "ui_MainWindow.h"
 
-MainWindow::MainWindow(QWidget *parent)
-    : QMainWindow(parent)
-    , ui(new Ui::MainWindow)
-{
+MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent), ui(new Ui::MainWindow) {
     ui->setupUi(this);
+    this->ui->log_operations->setReadOnly(true);
 }
 
-MainWindow::~MainWindow()
-{
+MainWindow::~MainWindow() {
     delete ui;
 }
 
+void MainWindow::on_open_folder_btn_clicked(){
+    this->folder_name = QFileDialog::getExistingDirectory(this, tr("Open folder"), "/home");
+    qDebug() << "Set folder_name : " << this->folder_name << '\n';
+    this->ui->log_operations->appendPlainText("Set folder_name : " + this->folder_name + "\n");
+
+    this->ui->label->setText(this->folder_name);
+
+    qDebug() << "Set label : " << this->folder_name << '\n';
+    this->ui->log_operations->appendPlainText("Set label : " + this->folder_name + "\n");
+}
+
+void MainWindow::on_send_btn_clicked() {
+    QString first_command = "git add " + this->folder_name;
+    this->ui->log_operations->appendPlainText("First command : " + first_command + "\n");
+    qDebug() << first_command << '\n';
+
+    QString second_command = "git commit -m \"" + this->ui->commit_text->text() + "\"";
+    this->ui->log_operations->appendPlainText("Second command : " + second_command + "\n");
+    qDebug() << second_command << '\n';
+
+    QString third_command = "git push";
+    this->ui->log_operations->appendPlainText("Third command : " + third_command + "\n");
+    qDebug() << third_command << '\n';
+
+    this->ui->log_operations->appendPlainText("Done!\n");
+    qDebug() << "Done!\n";
+}
--- MainWindow.h
+++ MainWindow.h
@@ -2,20 +2,28 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QFileDialog>
+#include <QDebug>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
 QT_END_NAMESPACE
 
-class MainWindow : public QMainWindow
-{
+class MainWindow : public QMainWindow {
     Q_OBJECT
 
 public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+private slots:
+    void on_open_folder_btn_clicked();
+
+    void on_send_btn_clicked();
+
 private:
     Ui::MainWindow *ui;
+    QString folder_name;
 };
+
 #endif // MAINWINDOW_H
